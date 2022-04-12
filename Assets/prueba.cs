646eb15edaa05f07c4d04ad613b80fd6ae8cf60e
Nachbert <102158970+Nachbert@users.noreply.github.com>
2022-04-12 10:35:41 -0300

    examen

---
+++ Assets/prueba.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prueba : MonoBehaviour
+
+{
+    public int campo;
+    public int platea;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int maxcampo = 20400;
+        int maxplatea = 16200;
+        int maxpredio = (maxplatea + maxcampo);
+        Debug.Log("Sobraron " + (maxpredio - campo + platea));
+        Debug.Log("Se vendieron un total de " + (campo + platea));
+
+        if (campo + platea > 18299)
+        {
+            Debug.Log("El festival fue un éxito!");
+        }
+
+        else
+        {
+            Debug.Log("Debemos mejorar la convocatoria :(");
+        }
+       
+           
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
